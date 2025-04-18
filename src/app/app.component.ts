2a5087b49f7eb07d0c7b658bfb05ab281f1fc6e8
Abdalla <90405765+GitAbdalla@users.noreply.github.com>
2025-04-18 22:54:50 +0200

    added form and about section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from "./about/about.component";
+import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , HeaderComponent ,HeroComponent ],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, AboutComponent ,FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
