6215d550a83a7f0ff667f0c62214ee61f8475ed9
Abdalla <90405765+GitAbdalla@users.noreply.github.com>
2025-04-18 23:04:35 +0200

    added services and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from "./about/about.component";
 import { FormComponent } from './form/form.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServicesComponent } from './services/services.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, AboutComponent ,FormComponent],
+  imports: [ HeaderComponent, HeroComponent, AboutComponent ,FormComponent , FooterComponent , ServicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
